2c5c8cad13c4699ddc4a7c7dc49607b61c9c09aa
Igor <igorgmariano010@outlook.com>
2025-01-28 08:36:21 -0300

    chore: Inserção de animações

--- src/components/QuizCard/index.tsx
+++ src/components/QuizCard/index.tsx
@@ -1,34 +1,39 @@
-import { TouchableOpacity, TouchableOpacityProps, Text, View } from 'react-native';
+import {TouchableOpacity, TouchableOpacityProps, Text, View} from 'react-native';
 
-import { styles } from './styles';
-import { THEME } from '../../styles/theme';
+import {styles} from './styles';
+import {THEME} from '../../styles/theme';
 
-import { LevelBars } from '../LevelBars';
-import { QUIZZES } from '../../data/quizzes';
+import {LevelBars} from '../LevelBars';
+import {QUIZZES} from '../../data/quizzes';
+import Animated, {FadeIn} from "react-native-reanimated";
+
+const TouchableOpacityAnimated = Animated.createAnimatedComponent(TouchableOpacity)
 
 type Props = TouchableOpacityProps & {
-  data: typeof QUIZZES[0];
+    data: typeof QUIZZES[0];
+    index: number;
 }
 
-export function QuizCard({ data, ...rest }: Props) {
-  const Icon = data.svg;
+export function QuizCard({index, data, ...rest}: Props) {
+    const Icon = data.svg;
 
-  return (
-    <TouchableOpacity
-      style={styles.container}
-      {...rest}
-    >
-      <View style={styles.header}>
-        <View style={styles.iconContainer}>
-          {Icon && <Icon size={24} color={THEME.COLORS.GREY_100} />}
-        </View>
+    return (
+        <TouchableOpacityAnimated
+            entering={FadeIn.delay(index * 100)}
+            style={styles.container}
+            {...rest}
+        >
+            <View style={styles.header}>
+                <View style={styles.iconContainer}>
+                    {Icon && <Icon size={24} color={THEME.COLORS.GREY_100}/>}
+                </View>
 
-        <LevelBars level={data.level} />
-      </View>
+                <LevelBars level={data.level}/>
+            </View>
 
-      <Text style={styles.title}>
-        {data.title}
-      </Text>
-    </TouchableOpacity>
-  );
+            <Text style={styles.title}>
+                {data.title}
+            </Text>
+        </TouchableOpacityAnimated>
+    );
 }
